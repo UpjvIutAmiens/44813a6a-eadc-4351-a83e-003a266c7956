238afc3360b49930c0bd069e30c9cd1fd91f1751
jpdemory <jpdemory@manastria.fr>
2018-02-02 17:04:04 +0100

    Création du projet Demo

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Afficher la table des 8 (multiplication)
+            // Concaténation : +
+            // Saut de ligne : \n
+
+
+        }
+    }
+}
